66f5831d2aa773dda2132c836adcfab870d09bf9
Tobias Koch <kochtobi@users.noreply.github.com>
2024-03-28 16:07:45 +0100

    Adjust openbis timeout

--- openbis-connector/src/main/java/life/qbic/data_download/openbis/ApiV3.java
+++ openbis-connector/src/main/java/life/qbic/data_download/openbis/ApiV3.java
@@ -3,20 +3,25 @@ package life.qbic.data_download.openbis;
 import ch.ethz.sis.openbis.generic.asapi.v3.IApplicationServerApi;
 import ch.ethz.sis.openbis.generic.dssapi.v3.IDataStoreServerApi;
 import ch.systemsx.cisd.common.spring.HttpInvokerUtils;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * A provider for openbis apis v3
  */
 public class ApiV3 {
 
+  private static final Duration TIMEOUT = Duration.of(5, ChronoUnit.DAYS);
+
   private ApiV3() {
     //hide the implicit constructor
   }
+
   public static IApplicationServerApi applicationServer(String url) {
-    return HttpInvokerUtils.createServiceStub(IApplicationServerApi.class, url + IApplicationServerApi.SERVICE_URL, 100_000L);
+    return HttpInvokerUtils.createServiceStub(IApplicationServerApi.class, url + IApplicationServerApi.SERVICE_URL, TIMEOUT.toMillis());
   }
   public static IDataStoreServerApi dataStoreServer(String url) {
-    return HttpInvokerUtils.createStreamSupportingServiceStub(IDataStoreServerApi.class, url + IDataStoreServerApi.SERVICE_URL, 100_000L);
+    return HttpInvokerUtils.createStreamSupportingServiceStub(IDataStoreServerApi.class, url + IDataStoreServerApi.SERVICE_URL, TIMEOUT.toMillis());
   }
 
 }
